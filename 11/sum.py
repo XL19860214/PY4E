8bf4e634abbaee9539222911179a9873f3bdba18
Xuwei Li <dev@xuwei.li>
2021-10-16 14:17:20 +0000

    Fix indent.

--- 11/sum.py
+++ 11/sum.py
@@ -2,13 +2,13 @@ import re
 
 inputFilename = input("Filename: ")
 if len(inputFilename) < 1:
-  inputFilename = "regex_sum_1362834.txt"
+    inputFilename = "regex_sum_1362834.txt"
 
 file = open(inputFilename)
 fileContent = file.read()
 numbers = re.findall('[0-9]+', fileContent)
 sum = 0
 for number in numbers:
-  sum += int(number)
+    sum += int(number)
 
 print(sum)
